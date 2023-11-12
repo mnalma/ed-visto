f35393c14e70dcf017e95eb6f756172746d331dc
Maite Natalia Alma <nat_alma@hotmail.com>
2023-11-12 19:01:01 -0300

    Agregado de menu nav

--- BarraDeNavegacion.js
+++ BarraDeNavegacion.js
@@ -1,32 +1,46 @@
-const botonFeed = document.getElementById('botonFeed')
-const botonBusquedaAnalisis = document.getElementById('botonBusquedaAnalisis')
-const botonCrearColaborar = document.getElementById('botonCrearColaborar')
-const botonNavegacion = document.querySelectorAll('.botonNavegacion')
+const profileMenuBtn = document.getElementById('profile-menu-button');
+const userMenu = document.getElementById('user-menu');
 
-botonFeed.addEventListener('click', function(){
-    botonNavegacion.forEach((btn) => {
-        btn.classList.remove('activo');
-    });
-    this.classList.add('activo');
+const showHideMenu = () => {
+    if(userMenu.classList.contains('hidden')){
+        userMenu.classList.remove('hidden');
+    } else {
+        userMenu.classList.add('hidden');
+    }
+}
 
-    console.log('Le diste click al botón Feed');
-})
+profileMenuBtn.addEventListener('click', showHideMenu);
 
 
-botonBusquedaAnalisis.addEventListener('click', function(){
-    botonNavegacion.forEach((btn) => {
-        btn.classList.remove('activo');
-    });
-    this.classList.add('activo');
+// const botonFeed = document.getElementById('botonFeed')
+// const botonBusquedaAnalisis = document.getElementById('botonBusquedaAnalisis')
+// const botonCrearColaborar = document.getElementById('botonCrearColaborar')
+// const botonNavegacion = document.querySelectorAll('.botonNavegacion')
+
+// botonFeed.addEventListener('click', function(){
+//     botonNavegacion.forEach((btn) => {
+//         btn.classList.remove('activo');
+//     });
+//     this.classList.add('activo');
+
+//     console.log('Le diste click al botón Feed');
+// })
+
+
+// botonBusquedaAnalisis.addEventListener('click', function(){
+//     botonNavegacion.forEach((btn) => {
+//         btn.classList.remove('activo');
+//     });
+//     this.classList.add('activo');
         
-    console.log('Le diste click al botón búsqueda y análisis');
-})
+//     console.log('Le diste click al botón búsqueda y análisis');
+// })
 
-botonCrearColaborar.addEventListener('click', function(){
-    botonNavegacion.forEach((btn) => {
-        btn.classList.remove('activo');
-    });
-    this.classList.add('activo');
+// botonCrearColaborar.addEventListener('click', function(){
+//     botonNavegacion.forEach((btn) => {
+//         btn.classList.remove('activo');
+//     });
+//     this.classList.add('activo');
 
-    console.log('Le diste click al botón crear y colaborar');
-})
+//     console.log('Le diste click al botón crear y colaborar');
+// })
