2657604a3257e0edea668da15a75e08a0326207e
Maite Natalia Alma <nat_alma@hotmail.com>
2023-11-27 20:55:25 -0300

    Mensajes de las validaciones, pero solo si las muestro desde comienzo y al completar bien el campo desaparecen.

--- JS/perfilEstudiante.js
+++ JS/perfilEstudiante.js
@@ -71,13 +71,15 @@ function onResetClick() {
 // return true;
 // } 
 
-function validar(){
-document.getElementById("validationForm").addEventListener("onsubmitButton", function(event) {
+// function validar(){
+document.getElementById("validationForm").addEventListener("submit", function(event) {
     var nombreInput = document.getElementById("input-name");
     var apellidoInput = document.getElementById("input-apellido");
+    var dateInput = document.getElementById("date-usuario");
 
     var nombreError = document.getElementById("nombreError");
     var apellidoError = document.getElementById("apellidoError");
+    var dateError = document.getElementById("dateError");
 
     if (!nombreInput.checkValidity()) {
         nombreError.style.display = "block";
@@ -92,8 +94,25 @@ document.getElementById("validationForm").addEventListener("onsubmitButton", fun
     } else {
         apellidoError.style.display = "none";
     }
-});
+    if (!dateInput.checkValidity()) {
+        dateError.style.display = "block";
+        event.preventDefault();
+    } else {
+        dateError.style.display = "none";
+    }
+    if (nombreInput.checkValidity() && apellidoInput.checkValidity() && dateInput.checkValidity()) {
+        onsubmitButton();
 }
+
+});
+
+document.getElementById("input-name").addEventListener("input", function() {
+    document.getElementById("nombreError").style.display = "none";
+});
+
+document.getElementById("input-apellido").addEventListener("input", function() {
+    document.getElementById("apellidoError").style.display = "none";
+});
 
 
 //Boton carga foto de perfil
@@ -106,40 +125,40 @@ document.getElementById("validationForm").addEventListener("onsubmitButton", fun
 // openModalBtn.addEventListener('click', printPantalla);
 
 //boton de carga de foto
-  const fileInput = document.getElementById('fileInput');
-  const customButton = document.getElementById('custom-button');
+//   const fileInput = document.getElementById('fileInput');
+//   const customButton = document.getElementById('custom-button');
 
-  customButton.addEventListener('click', () => {
-    fileInput.click();
-  });
+//   customButton.addEventListener('click', () => {
+//     fileInput.click();
+//   });
 
-document.addEventListener('DOMContentLoaded', function () {
-    const openModalBtn = document.getElementById('openModalBtn');
-    const imageModal = document.getElementById('imageModal');
-    const closeModalBtn = document.getElementById('closeModalBtn');
-    const fileInput = document.getElementById('fileInput');
-    const previewImage = document.getElementById('previewImage');
+// document.addEventListener('DOMContentLoaded', function () {
+//     const openModalBtn = document.getElementById('openModalBtn');
+//     const imageModal = document.getElementById('imageModal');
+//     const closeModalBtn = document.getElementById('closeModalBtn');
+//     const fileInput = document.getElementById('fileInput');
+//     const previewImage = document.getElementById('previewImage');
 
-    openModalBtn.addEventListener('click', function () {
-        imageModal.style.display = 'block';
-    });
+//     openModalBtn.addEventListener('click', function () {
+//         imageModal.style.display = 'block';
+//     });
 
-    closeModalBtn.addEventListener('click', function () {
-        imageModal.style.display = 'none';
-    });
+//     closeModalBtn.addEventListener('click', function () {
+//         imageModal.style.display = 'none';
+//     });
 
-    fileInput.addEventListener('change', function () {
-        const file = fileInput.files[0];
+//     fileInput.addEventListener('change', function () {
+//         const file = fileInput.files[0];
 
-        if (file) {
-            const reader = new FileReader();
+//         if (file) {
+//             const reader = new FileReader();
 
-            reader.addEventListener('load', function () {
-                previewImage.src = reader.result;
-                previewImage.style.display = 'block';
-            });
+//             reader.addEventListener('load', function () {
+//                 previewImage.src = reader.result;
+//                 previewImage.style.display = 'block';
+//             });
 
-            reader.readAsDataURL(file);
-        }
-    });
-});
+//             reader.readAsDataURL(file);
+//         }
+//     });
+// });
