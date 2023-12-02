6923d64dd195783aa8361af43cb5cd10578ee5c8
Maite Natalia Alma <nat_alma@hotmail.com>
2023-12-02 01:01:26 -0300

    Mandar a revision

--- JS/perfilEstudiante.js
+++ JS/perfilEstudiante.js
@@ -12,7 +12,7 @@ const showHideMenu = () => {
 
 profileMenuBtn.addEventListener('click', showHideMenu);
 
-
+//Botones personalizados con sweetAlert
 function showSuccess() {
     Swal.fire({
         title: "Guardar Formulario",
@@ -31,44 +31,59 @@ function onResetClick() {
     
 }
 
-// document.getElementById("validationForm").addEventListener("submit", function(event)) {
-    var nombre = document.getElementById("input-name");
-    var apellido = document.getElementById("input-apellido");
-    var nacimiento = document.getElementById("date-usuario");
+//Formulario
+var nombre = document.getElementById("input-name");
+var apellido = document.getElementById("input-apellido");
+var nacimiento = document.getElementById("date-usuario");
+var mail = document.getElementById("mail_usuario");
 
-    var nombreError = document.getElementById("nombreError");
-    var apellidoError = document.getElementById("apellidoError");
-    var nacimientoError = document.getElementById("nacimientoError");
+var nombreError = document.getElementById("nombreError");
+var apellidoError = document.getElementById("apellidoError");
+var nacimientoError = document.getElementById("nacimientoError");
+var mailError = document.getElementById("mailError");
 
-    const form = document.getElementById("validationForm");
+const form = document.getElementById("validationForm");
+var lettersRegex = /^[A-Za-záéíóúüñÁÉÍÓÚÜÑ\s]+$/;
 
-    form.addEventListener('submit', (event)=>{
-        event.preventDefault();
-        if(!validateEmpty(nombre.value, nombre, nombreError, 'nombre')) return false;
-        if(!validateEmpty(apellido.value, apellido, apellidoError, 'apellido')) return false;
-        if(!validateEmpty(nacimiento.value, nacimiento, nacimientoError, 'fecha de nacimiento'))return false;
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (!validatePattern(nombre.value, nombre, nombreError, 'nombre', lettersRegex)) return false;
+    if (!validatePattern(apellido.value, apellido, apellidoError, 'apellido', lettersRegex)) return false;
+    if (!validateEmpty(nacimiento.value, nacimiento, nacimientoError, 'fecha de nacimiento')) return false;
+    if (!validateEmpty(mail.value, mail, mailError, 'e-mail')) return false;
 
-        showSuccess();
-        return true;
-    });
+    showSuccess();
+    return true;
+});
 
+// Validación de que el campo no esté vacío y cumpla con el patrón
+function validatePattern(valueInput, divInput, divError, nombreInput, lettersRegex) {
+    if (!validateEmpty(valueInput, divInput, divError, nombreInput)) return false;
 
-// validación general de que el campo no este vacio   
-   function validateEmpty(valueInput, divInput, divError, nombreInput){
-    if(valueInput.length == 0){
-        showError(divInput, divError, nombreInput);
+    if (!lettersRegex.test(valueInput)) {
+        showError(divInput, divError, `El ${nombreInput} no debe contener números ni caracteres especiales`);
         return false;
-    }else{
+    } else {
         hideError(divError);
         return true;
     }
-   }
+}
 
-   function showError(divInput, divError, nombreInput){
-    divError.innerHTML = `<p class= "error">El campo ${nombreInput} debe ser completado </p>`
-   }
+function validateEmpty(valueInput, divInput, divError, nombreInput) {
+    if (valueInput.trim().length === 0) {
+        showError(divInput, divError, `El ${nombreInput} debe ser completado`);
+        return false;
+    } else {
+        hideError(divError);
+        return true;
+    }
+}
 
-   function hideError(divError){
-    divError.innerHTML = ""
-   }
+function showError(divInput, divError, message) {
+    divError.innerHTML = `<p class="error">${message}</p>`;
+}
+
+function hideError(divError) {
+    divError.innerHTML = "";
+}
 
